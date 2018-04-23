69acaa1d704d2f3cbece0e1649fa7aaced758f7b
AlexLiber <liberaleksa@gmail.com>
2018-04-23 23:12:55 +0200

    Create project files

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var buttonClass = document.getElementsByClassName('button');
+console.log(buttonClass);
+
+console.log(buttonClass.length);
+
+for (var i = 0; i < buttonClass.length; i++) {
+    alert(buttonClass[i].innerText);
+    console.log(buttonClass[i].innerText);
+}
